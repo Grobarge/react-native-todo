1bed864feb146743be197359e5a754ba5db03966
Gaje Robarge <gajer@paulmitchell.edu>
2020-01-13 12:53:02 -0700

    so far

--- App.js
+++ App.js
@@ -1,19 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './Components/Header';
+import InputBar from './Components/InputBar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todoInput: '',
+      todos: [
+        { id: 0, title: 'Take Out the trash', done: false },
+        { id: 1, title: 'Cook dinner', done: false }
+      ]
+    }
+  }
+
+  addNewTodo() {
+    let todos = this.state.todos;
+
+    todos.unshift({
+      id: todos.length + 1,
+      todo: this.state.todoInput,
+      done: false
+    });
+
+    this.setState({
+      todos,
+      todoInput: ''
+    });
+  }
+
+
+
+
+  render() {
+    // checking which platform , if ios display the statusbar component
+    const statusbar = (Platform.OS == 'ios') ? <View style={styles.statusbar}></View> : <View></View>;
+
+
+    return (
+      <View style={styles.container}>
+        {statusbar}
+
+        <Header title="Shit to do" />
+
+        <InputBar
+          textChange={todoInput => this.setState({ todoInput })}
+          addNewtodo={() => this.addNewTodo()}
+        />
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  statusbar: {
+    backgroundColor: 'black',
+    height: 45
+  }
 });
